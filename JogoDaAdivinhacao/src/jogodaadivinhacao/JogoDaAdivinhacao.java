8e04901c181ffc45baa2099c5fbb26b3b8624c49
flaviadsilva <flavvs2@gmail.com>
2024-05-27 17:49:15 -0300

    jogo da adivinhacao

--- JogoDaAdivinhacao/src/jogodaadivinhacao/JogoDaAdivinhacao.java
+++ JogoDaAdivinhacao/src/jogodaadivinhacao/JogoDaAdivinhacao.java
@@ -1,21 +1,78 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jogodaadivinhacao;
 
-/**
- *
- * @author aluno.saolucas
- */
+import java.util.Random;
+import java.util.Scanner;
+
 public class JogoDaAdivinhacao {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       do {
+            Scanner obj = new Scanner(System.in);
+            Random random = new Random();
+
+            String nome;
+            int numero = 0;
+            int tentativas = 0;
+            int aleatorio = random.nextInt(10);
+            int afirmacao;
+
+            System.out.println("Digite o nome do jogador:");
+            nome = obj.nextLine();
+            System.out.println("Olá, " + nome);
+            System.out.println("Você realmente quer jogar? Digite 1 para SIM e 2 para NÃO");
+            afirmacao = obj.nextInt();
+
+            if (afirmacao == 1) {
+                System.out.println("Vamos começar!");
+
+            } else if (afirmacao == 2) {
+                System.out.println("Obrigada, tchau!");
+                break;
+            }
+            while (numero != aleatorio) {
+
+                tentativas++;
+                System.out.println("Digite um número de 1 a 15");
+                numero = obj.nextInt();
+
+                System.out.println(" ");
+                
+                if (numero == aleatorio) {
+                    System.out.println("Você acertou! Parabéns");
+                    
+                    System.out.println(" ");
+                    
+                } else if (numero < aleatorio) {
+                    System.out.println("Dica: Tente um número maior");
+
+                    System.out.println(" ");
+                    
+                } else if (numero > aleatorio) {
+                    System.out.println("Dica: Tente um número menor");
+                    
+                    System.out.println(" ");
+                }
+                System.out.println("Você tentou " + tentativas + " vezes");
+                
+                System.out.println(" ");
+            }
+            System.out.println("Deseja continuar? Digite 1 para SIM e 2 para NÃO");
+            afirmacao = obj.nextInt();
+
+            if (afirmacao == 1) {
+                System.out.println("Vamos Recomeçar!");
+
+            } else if (afirmacao == 2) {
+                 System.out.println("Obrigada, tchau " + nome + "!");
+                break;
+            
+            }
+               
+        }while (true);
+       
     }
+        
+
+
     
 }
